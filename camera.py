7f90cc44ae8c50614308c7376112bc0a26251bff
sybren-marechal <sybrenmarechal@icloud.com>
2019-03-04 12:53:08 +0200

    fix code to GPIO

--- camera.py
+++ camera.py
@@ -1,49 +1,46 @@
-from gpiozero import LED
-from time import sleep
-from signal import pause
-from gpiozero import Button
-import requests
+import RPi.GPIO as GPIO  
+from time import sleep     # this lets us have a time delay (see line 15)  
+GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
+GPIO.setup(4, GPIO.IN)     
+GPIO.setup(17, GPIO.IN)   
+GPIO.setup(27, GPIO.IN)   
 
 URL = "http://172.17.2.10:3000/api/imageReader"
+  
+try:  
+    while True:            # this will carry on until you hit CTRL+C  
+        if GPIO.input(4): 
+            print "2"
+            data = {
+            "devID": "imageReader",
+            "imageMessage": "2"
+            }  
+            sleep(5)         
+        elif GPIO.input(17):
+            data = {
+            "devID": "imageReader",
+            "imageMessage": "3"
+            } 
+            print "3"  
+            sleep(5)         
+        elif GPIO.input(27): 
+            data = {
+            "devID": "imageReader",
+            "imageMessage": "4"
+            }
+            print "4"    
+            sleep(5)         
+        else:  
+            print "NO NUMBER"  
+        sleep(5)     
 
-
-button2 = Button(4)
-button3 = Button(17)
-button4 = Button(27)
-
-led2 = LED(18)
-led3 = LED(23)
-led4 = LED(24)
-
-while True:
+        try:
+            response = requests.request("POST", URL, data=data)
+            print(response.text)
+        except:
+            print("data not POSTED")
+            print(data)
     
-    if button2.is_pressed and button3.is_pressed and button4.is_pressed:
-        print("SELECT ANY ANSWER")
-        sleep(4)
-    elif button3.is_pressed and button4.is_pressed:
-        print("Answer 2")
-        data = {
-        "devID": "imageReader",
-        "imageMessage": "2"
-        }
-        sleep(4)
-    elif button2.is_pressed and button4.is_pressed:
-        print("Answer 3")
-        data = {
-        "devID": "imageReader",
-        "imageMessage": "3"
-        }
-        sleep(4)
-    elif button2.is_pressed and button3.is_pressed:
-        print("Answer 4")
-        data = {
-        "devID": "imageReader",
-        "imageMessage": "4"
-        }
-        sleep(4)
-    try:
-        response = requests.request("POST", URL, data=data)
-        print(response.text)
-    except:
-        print("data not POSTED")
-        print(data)
+  
+finally:                   # this block will run no matter how the try block exits  
+    GPIO.cleanup()         # clean up after yourself  
